20ddf891ba163808c97ccac9bfe667338d9d2882
MixaTret <mikhail.tretsiakevich@gmail.com>
2023-09-08 15:40:46 +0100

    init: script of function towel sort was writting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+
+  if (!matrix || matrix.length === 0) {
+    return [];
+  }
+
+  const newArray = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    if (i % 2 === 0) {
+      Array.prototype.push.apply(newArray, matrix[i]);
+    } else {
+      Array.prototype.push.apply(newArray, matrix[i].reverse());
+    }
+  }
+
+  return newArray;
 }
+
+
